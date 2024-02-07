10ad86343aa4b6f1f1f4cd3f7f7aab654e6b9a46
lauritikka <159169926+lauritikka@users.noreply.github.com>
2024-02-07 14:26:34 +0200

    turhat poistettu

--- Input.js
+++ Input.js
@@ -3,11 +3,9 @@ function sendMessage() {
     var message = document.getElementById("text-field").value;
 
     
-    // Checks if the message not empty
-    if (message.trim() !== "") {
     //send to server here, now console just logs.
     console.log("Message sent:", message);
-    }
+
      
     // Clears the text-field after sending the message.
     document.getElementById("text-field").value = "";
