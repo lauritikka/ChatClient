452a3e2f3961c522a755103ed662267a1325d8fe
lauritikka <159169926+lauritikka@users.noreply.github.com>
2024-02-07 19:49:58 +0200

    jotain

--- Input.js
+++ Input.js
@@ -2,10 +2,8 @@ const socket = new WebSocket('ws://192.168.0.12:6969');
 
 
 function sendMessage() {
-    // Get the value from the input field.
     var message = document.getElementById("text-field").value;
-  
-    // Clears the text-field after sending the message.
+    console.log (message);
     document.getElementById("text-field").value = "";
 
     socket.send(message);
@@ -16,6 +14,8 @@ socket.onmessage = function (message) {
     var messageContainer = document.getElementById("message-container");
     const newMessage = document.createElement('p');
     newMessage.textContent = message.data;
+    console.log (message.data);
     messageContainer.appendChild(newMessage);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
 }
 
