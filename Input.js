56691cbd379459674fc02610ed6a92b5b61e36cf
lauritikka <159169926+lauritikka@users.noreply.github.com>
2024-02-07 20:21:30 +0200

    joo

--- Input.js
+++ Input.js
@@ -4,6 +4,9 @@ const socket = new WebSocket('ws://192.168.0.12:6969');
 function sendMessage() {
     var message = document.getElementById("text-field").value;
     console.log (message);
+    var name = document.getElementById("text-field2").value;
+    console.log (name);
+
     document.getElementById("text-field").value = "";
 
     socket.send(message);
