e0b3bee40cf840a3e8553200f6dde409e3c798b7
lauritikka <159169926+lauritikka@users.noreply.github.com>
2024-02-07 18:51:22 +0200

    viestit kulkee

--- Input.js
+++ Input.js
@@ -1,12 +1,21 @@
+const socket = new WebSocket('ws://192.168.0.12:6969');
+
+
 function sendMessage() {
     // Get the value from the input field.
     var message = document.getElementById("text-field").value;
-
-    
-    //send to server here, now console just logs.
-    console.log("Message sent:", message);
-
-     
+  
     // Clears the text-field after sending the message.
     document.getElementById("text-field").value = "";
+
+    socket.send(message);
 }
+
+
+socket.onmessage = function (message) {
+    var messageContainer = document.getElementById("message-container");
+    const newMessage = document.createElement('p');
+    newMessage.textContent = message.data;
+    messageContainer.appendChild(newMessage);
+}
+
